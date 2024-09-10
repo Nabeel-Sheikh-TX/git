52b54cd31e437cef4ffa92897f8e73461194be91
Nabeel-Sheikh-TX <nabeelinayat164@gmail.com>
2024-09-10 03:32:41 -0700

    first

---
+++ 02-Datatypes.js
@@ -0,0 +1,30 @@
+let name = "Nabeel"
+let age  = 20
+let city = "lahore"
+
+//string = ""
+//number = 2 to power 53
+//bignit = 
+//bolean = true/false
+//undefined = undefined nodeno
+//null = standalone Value And its an Object
+// object
+
+
+
+console.log(typeof null );
+
+
+
+
+
+
+
+//number = 2 to 53
+//bignit = 
+//Boolean = True/False
+//Object = 
+//underfined = undefined
+//null = its an standalone value and Object
+// symbol = its an unique 
+//string = ""
---
+++ test.js
@@ -0,0 +1,6 @@
+const accountID = 11223344
+let password ="123456"
+var cityname = "lahore"
+
+console.log(accountID);
+console.table([accountID ,password ,cityname])
